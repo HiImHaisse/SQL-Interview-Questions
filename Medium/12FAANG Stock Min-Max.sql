fed3189543d8c4394fc38e18dcdf204c0a0cd716
HiHaisse <83473250+HiImHaisse@users.noreply.github.com>
2025-09-14 00:08:37 -0500

    Update 12FAANG Stock Min-Max.sql
    Optimizado

--- Medium/12FAANG Stock Min-Max.sql
+++ Medium/12FAANG Stock Min-Max.sql
@@ -3,16 +3,13 @@ SELECT
 ticker, to_char(date,'Mon-YYYY') as highest_mth, open as highest_open,
 row_number() over(PARTITION by ticker order by open desc) as ranking
 from stock_prices 
-order by ranking asc), t2 as (
-SELECT
-ticker, to_char(date,'Mon-YYYY') as lowest_mth, low as lowest_open,
-row_number() over(PARTITION by ticker order by open ASC) as ranking
+order by ranking asc), t2 as (select
+ticker, to_char(date,'Mon-YYYY') as lowest_mth, open as lowest_open,
+row_number() over(PARTITION by ticker order by open asc) as ranking
 from stock_prices 
-order by ranking asc), t3 as (
-select ticker,highest_mth,highest_open from t1
-where ranking=1), t4 as (
-select ticker,lowest_mth,lowest_open from t2
-where ranking=1) 
-select t2.ticker,t1.highest_mth,t1.highest_open,t2.lowest_mth,t2.lowest_open
-from t1
-inner join t2 on t1.ticker=t2.ticker
+order by ranking asc)
+select t1.ticker as ticker, t1.highest_mth, t1.highest_open,
+t2.lowest_mth, t2.lowest_open from t1
+JOIN t2 on t1.ticker=t2.ticker
+where t1.ranking =1 and t2.ranking=1
+order by ticker asc
